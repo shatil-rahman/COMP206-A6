7f6726a83795efd3e849999417c59aaa15da7660
Shatil RAHMAN <srahma34@ds2.CS.McGill.CA>
2017-12-03 19:24:53 -0500

    created and set up main.c, which contains the forked calls to producer and consumer

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+//Name 1: Shatil Rahman
+//ID: 260606042
+
+//Name 2: Adeeb Ibne Amjad
+//ID: 260625272
 
 int main(){
 
-	
+	int status;
+	int pid = fork();
+	if( pid == -1) exit(1);
+
+	FILE *turn = fopen("TURN.txt", "w");
+
+	fprintf(turn, "0");
+
+	fclose(turn);
+
+	if(pid == 0){
+		consumer();
+		//wait();
+	}
+
+	if(pid != 0){
+		producer();
+		wait(&status);
+	}
 
 
 
